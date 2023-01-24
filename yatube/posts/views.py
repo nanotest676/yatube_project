5b7503fbdf9a9e9a612ad4bad4aec080f5dfbc25
Никита Кузнецов <nickbatmanll@gmail.com>
2023-01-24 17:11:48 +0300

    furst html

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,12 @@
 # Create your views here.
+from django.shortcuts import render
 from django.http import HttpResponse
 
 
 # Главная страница
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    return render(request, template)
 
 
 # Страница со списком мороженого
